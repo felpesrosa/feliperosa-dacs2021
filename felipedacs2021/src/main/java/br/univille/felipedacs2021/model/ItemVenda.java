db35e8a0ef801092161fd74966ea23220d321629
Felipe Rosa <felpesrosa@gmail.com>
2021-06-02 01:23:47 +0000

    criar e modelar as classes

--- felipedacs2021/src/main/java/br/univille/felipedacs2021/model/ItemVenda.java
+++ felipedacs2021/src/main/java/br/univille/felipedacs2021/model/ItemVenda.java
@@ -1,32 +1,38 @@
 package br.univille.felipedacs2021.model;
 
+import java.util.ArrayList;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class ItemVenda {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY);
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    private Produto produto;
-
+    private float valor;
+    private float valorDesconto;
+    
     public long getId() {
         return id;
     }
-
     public void setId(long id) {
         this.id = id;
     }
-
-    public Produto getProduto() {
-        return produto;
+    public float getValor() {
+        return valor;
     }
-
-    public void setProduto(Produto produto) {
-        this.produto = produto;
+    public void setValor(float valor) {
+        this.valor = valor;
     }
-    
+    public float getValorDesconto() {
+        return valorDesconto;
+    }
+    public void setValorDesconto(float valorDesconto) {
+        this.valorDesconto = valorDesconto;
+    }
     
 }
--- felipedacs2021/src/main/java/br/univille/felipedacs2021/model/Venda.java
+++ felipedacs2021/src/main/java/br/univille/felipedacs2021/model/Venda.java
@@ -1,33 +1,59 @@
 package br.univille.felipedacs2021.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GenerationType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import java.util.ArrayList;
+import java.util.Date;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import java.util.List;
+
+// essa classe deve ser persistida em um banco de dados
 @Entity
 public class Venda {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY);
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    private ItemVenda itemVenda;
+    private float valorTotal;
+    @Temporal(value = TemporalType.DATE)
+    private Date data;
+    @Column(length = 15)
+    private String cupom;
 
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "venda_id")
+    private List<ItemVenda> colItemVenda = new ArrayList<ItemVenda>();
+    
     public long getId() {
         return id;
     }
-
     public void setId(long id) {
         this.id = id;
     }
-
-    public ItemVenda getItemVenda() {
-        return itemVenda;
+    public float getValorTotal() {
+        return valorTotal;
     }
-
-    public void setItemVenda(ItemVenda itemVenda) {
-        this.itemVenda = itemVenda;
+    public void setValorTotal(float valorTotal) {
+        this.valorTotal = valorTotal;
     }
-
-
-    
+    public Date getData() {
+        return data;
+    }
+    public void setData(Date data) {
+        this.data = data;
+    }
+    public String getCupom() {
+        return cupom;
+    }
+    public void setCupom(String cupom) {
+        this.cupom = cupom;
+    }   
 }
