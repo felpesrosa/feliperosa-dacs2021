f8a78f1a23403fc80123929cfe116cdcbad8bd2f
Felipe <felpesrosa@gmail.com>
2021-10-20 01:07:26 +0000

    implementando segurança na interface

--- felipedacs2021/src/main/java/br/univille/felipedacs2021/model/Usuario.java
+++ felipedacs2021/src/main/java/br/univille/felipedacs2021/model/Usuario.java
@@ -7,7 +7,6 @@ import javax.persistence.Id;
 
 @Entity
 public class Usuario {
-    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
@@ -17,20 +16,24 @@ public class Usuario {
     public long getId() {
         return id;
     }
+
     public void setId(long id) {
         this.id = id;
     }
+
     public String getUsuario() {
         return usuario;
     }
+
     public void setUsuario(String usuario) {
         this.usuario = usuario;
     }
+
     public String getSenha() {
         return senha;
     }
+
     public void setSenha(String senha) {
         this.senha = senha;
     }
-    
 }
---
+++ felipedacs2021/src/main/java/br/univille/felipedacs2021/repository/UsuarioRepository.java
@@ -0,0 +1,14 @@
+package br.univille.felipedacs2021.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.univille.felipedacs2021.model.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+    Usuario findByUsuario(String usuario);
+
+    Usuario findByUsuarioAndSenha(String usuario, String senha);
+    
+}
